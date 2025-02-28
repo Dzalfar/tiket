1a4a8d87338f8cacc9102d2f50d34e4d30f3a500
4ll5olved <abrahamwibowo2@gmail.com>
2025-02-28 20:10:55 +0700

    Update eventform.tsx

--- components/ui/shared/eventform.tsx
+++ components/ui/shared/eventform.tsx
@@ -14,12 +14,10 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import EventFormSchema from '@/lib/validator'
+import { eventDefaultValues } from '@/constants'
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "username must be at least 2 characters."
-    }),
-});
+
 
 type EventFormProps = {
     userId: string;
@@ -27,39 +25,49 @@ type EventFormProps = {
   }
   
 const EventForm: React.FC<EventFormProps> = ({ userId, type }) => {
-      // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
+
+    const initialValues = eventDefaultValues;
+  const form = useForm<z.infer<typeof EventFormSchema>>({
+    resolver: zodResolver(EventFormSchema),
+    defaultValues: initialValues
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof EventFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
   return (
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5k">
+        <div className='flex flex-col gap-5 md:flex-row'>
+        <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+            <FormItem className='w-f'>
+                 <FormControl>
+                <Input placeholder="event title" {...field}  className='input-field' />
+                </FormControl>
+                <FormMessage />
+            </FormItem>
+            )}
+        />   
+        <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+            <FormItem className='w-f'>
+                 <FormControl>
+                <Input placeholder="event title" {...field}  className='input-field' />
+                </FormControl>
+                <FormMessage />
+            </FormItem>
+            )}
+                  />
+                  
+        </div>
       <Button type="submit">Submit</Button>
     </form>
   </Form> 
